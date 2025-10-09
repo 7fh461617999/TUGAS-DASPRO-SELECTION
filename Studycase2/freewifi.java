e6fcc546635e2444c8e75d9a2f29e7fc8e7608f6
habibi <habibifirman117@gmail.com>
2025-10-09 14:58:41 +0700

    study case 2

--- Studycase2/freewifi.java
+++ Studycase2/freewifi.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 public class freewifi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Input user type");
+        System.out.print("Input user type (lecturer/student/others) : ");
         String usertype = sc.nextLine();
-        if (usertype == "lecturer") {
+
+        if (usertype.equalsIgnoreCase("lecturer")) {
             System.out.println("Wifi granted access (lecturer)");
-        } else if (condition) {
-            
+
+        } else if (usertype.equalsIgnoreCase("student")) {
+         System.out.print("enter number of credits : ");
+         int credits = sc.nextInt();
+
+         if (credits >= 12) {
+            System.out.println("Wifi Access granted for student (active student)");
+
+         } else {System.out.println("Wifi access Denied, credits less than 12");}
+
         } else {
-            
-        }{
-            
+            System.out.println("ACCESS DENIED");
         }
     }
 }
