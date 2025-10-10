5cd2736c8719d7a4fc89a0e9e618793a3df8d476
habibi <habibifirman117@gmail.com>
2025-10-10 21:33:40 +0700

    modifikasi percobaan 2

--- jobsheet5/switchCetakKRS09.java
+++ jobsheet5/switchCetakKRS09.java
@@ -6,33 +6,26 @@ public class switchCetakKRS09 {
         System.out.println("--- Print KRS SIAKAD ---");
         System.out.print("Input now semester :");
         int semester = sc.nextInt();
-        switch (semester) {
-            case 1:
-               System.out.println("display KRS 1st semester"); 
-                break;
-            case 2 :
-               System.out.println("display KRS 2nd semester"); 
-                break;
-            case 3 :
-               System.out.println("display KRS 3rd semester"); 
-                break;
-            case 4 :
-               System.out.println("display KRS 4rd semester"); 
-                break;
-            case 5 :
-               System.out.println("display KRS 5rd semester"); 
-                break;
-            case 6:
-               System.out.println("display KRS 6rd semester"); 
-                break;
-            case 7 :
-               System.out.println("display KRS 7rd semester"); 
-                break;
-            case 8 :
-               System.out.println("display KRS 8rd semester"); 
-                break;
-            default:
-                break;
+        
+        if (semester == 1) {
+            System.out.println("display KRS 1st semester"); 
+        } else if (semester == 2) {
+             System.out.println("display KRS 2nd semester"); 
+        } else if (semester == 3) {
+             System.out.println("display KRS 3rd semester"); 
+        } else if (semester == 4) {
+             System.out.println("display KRS 4rd semester"); 
+        } else if (semester == 5) {
+             System.out.println("display KRS 5rd semester"); 
+        } else if (semester == 6) {
+             System.out.println("display KRS 6rd semester"); 
+        } else if (semester == 7) {
+             System.out.println("display KRS 7rd semester"); 
+        } else if (semester == 8) {
+             System.out.println("display KRS 8rd semester"); 
+        } else {
+            
+            System.out.println("Invalid semester input !!");
         }
     }
 }
