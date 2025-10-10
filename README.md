8e66695b57a9c66d2517e7056340cc51c2acdd76
habibi <habibifirman117@gmail.com>
2025-10-10 07:19:35 +0700

    percobaan 3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # TUGAS-DASPRO-SELECTION
+ name : *Firman habibi*
+ nim  : 254107020161 
+ prodi : D4 Teknik informatika
+ absen number : 09
--- jobsheet5/nestedUjianSkripsi09.java
+++ jobsheet5/nestedUjianSkripsi09.java
@@ -2,14 +2,17 @@ package jobsheet5;
 import java.util.Scanner;
 public class nestedUjianSkripsi09 {
     public static void main(String[] args) {
+//declaration
         Scanner sc = new Scanner(System.in);
         String message;
+        //input
         System.out.println("has a student been cleared of compensation ? yes or no");
         String compensationStatus = sc.nextLine().trim();
         System.out.println("Enter the number of guidance logs with supervisor 1: ");
         int supervisor1 =sc.nextInt();
         System.out.println("Enter the number of guidance logs with supervisor 2: ");
         int supervisor2 =sc.nextInt();
+        //proses
         if (compensationStatus.equalsIgnoreCase("yes")) {
             if (supervisor1 >= 8 && supervisor2 >=4) {
                 message="are reqeriments are met. the student may register for the thesis exam";
@@ -23,6 +26,7 @@ public class nestedUjianSkripsi09 {
         } else {
             message="Failed : the student has not been cleared of comparition";
         }
+        //output
         System.out.println(message);
     }
 }
