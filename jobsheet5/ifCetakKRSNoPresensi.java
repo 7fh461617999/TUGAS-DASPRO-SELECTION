abfbc89364d432dcbc4a0fecb0b99c6ceb6fa18a
habibi <habibifirman117@gmail.com>
2025-10-10 09:37:01 +0700

    task1

--- jobsheet5/ifCetakKRSNoPresensi.java
+++ jobsheet5/ifCetakKRSNoPresensi.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 public class ifCetakKRSNoPresensi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//input
+
         System.out.print("Is your UKT paid in full? (true/false)? ");
         boolean uktLunas = sc.nextBoolean();
 
         System.out.println(
             uktLunas
-            ? "UKT payment verified,please print KRS and request DPA signature"
-            : "UKT payment unverified,sorry you can't got KRS"
+            ? "UKT payment verified, please print KRS and request DPA signature"
+            : "UKT payment unverified, sorry you can't got KRS"
         );
 
         sc.close();
